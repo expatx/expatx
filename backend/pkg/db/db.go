92c32752c81548ae96b9d1ccecb7024f4c9d9ad6
jonmarc <j@jonmarc.dev>
2023-06-18 08:37:22 -0700

    feat(db): add some foundational code for mongo

--- backend/pkg/db/db.go
+++ backend/pkg/db/db.go
@@ -1,2 +1,39 @@
-// Package db provides database operations.
 package db
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Document interface{}
+
+// DB represents a MongoDB instance
+type DB struct {
+	Client *mongo.Client
+}
+
+// NewDB creates a new DB instance
+func NewDB(uri string) (*DB, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, err
+	}
+	return &DB{Client: client}, nil
+}
+
+// GetCollection gets a collection from the database
+func (db *DB) GetCollection(database string, collection string) *mongo.Collection {
+	return db.Client.Database(database).Collection(collection)
+}
+
+// InsertDocument inserts a new document into the collection
+func (db *DB) InsertDocument(database string, collection string, doc any) (*mongo.InsertOneResult, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	return db.GetCollection(database, collection).InsertOne(ctx, doc)
+}
